2682103f645dc4bf19d61fc148b1b6fd6b2e57e3
Arunpraveen98 <arunfbfb78@gmail.com>
2023-04-15 00:45:27 +0530

    Image_added

--- src/components/Projectlist/Projectlist.js
+++ src/components/Projectlist/Projectlist.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./ProjectCards.css";
 import ProjectCard from "./ProjectCard";
 import { Container, Row, Col } from "react-bootstrap";
+import Zen_class from "../../Images/Zen-class.png";
 import web_scrapper from "../../Images/Web-Scrapper.png";
 import student_teacher_db from "../../Images/student-teacher.png";
 import pending from "../../Images/pending.webp";
 function Projectlist() {
+  console.log(process.env.REACT_APP_STUDENT_E_LEARNING_APP);
   return (
     <div className="projectbackground">
       {/* ----------------------------- */}
@@ -14,6 +16,20 @@ function Projectlist() {
         <Container>
           {/* ----------------------------- */}
           <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+            {/* ----------------------------- */}
+            <Col md={4} className="project-card">
+              <ProjectCard
+                imgPath={Zen_class}
+                isBlog={false}
+                title="E-Learning App"
+                description="This project is an innovative student e-learning app built using MERN stack, 
+                offering an immersive learning experience. Attend daily classes, complete daily tasks,
+                 access free courses, and raise queries for a seamless and interactive educational journey."
+                Demo_Link={`${process.env.REACT_APP_STUDENT_E_LEARNING_APP}`}
+                Front_end="https://github.com/Arunpraveen98/MERN-CAPSTONE-PROJECT-FRONTEND"
+                Back_end="https://github.com/Arunpraveen98/MERN-CAPSTONE-PROJECT-BACKEND"
+              />
+            </Col>
             {/* ----------------------------- */}
             <Col md={4} className="project-card">
               <ProjectCard
@@ -26,7 +42,7 @@ function Projectlist() {
                 side-by-side. The project utilizes Node.js and MongoDB for backend
                 processing, while React and Express provide an interactive frontend
                 experience."
-                Demo_Link="https://web-scrapper-e-commerce-arun.netlify.app/"
+                Demo_Link={`${process.env.REACT_APP_WEB_SCRAPPER_APP}`}
                 Front_end="https://github.com/Arunpraveen98/Web-Scrapper-Frontend"
                 Back_end="https://github.com/Arunpraveen98/Web-Scrapper-Backend"
               />
@@ -41,25 +57,13 @@ function Projectlist() {
                 using React.js. With its intuitive user interface, where Students
                 and Teachers can CREATE data, have access to the added data , EDIT
                 and UPDATE the data, and DELETE the data."
-                Demo_Link="https://student-teacher-db-arun.netlify.app/"
+                Demo_Link={`${process.env.REACT_APP_STUDENT_TEACHER_APP}`}
                 Front_end="https://github.com/Arunpraveen98/Student-Teacher-CRUD-Api"
                 Back_end=""
               />
             </Col>
             {/* ----------------------------- */}
-            {/* <Col md={4} className="project-card">
-              <ProjectCard
-                imgPath={student_db}
-                isBlog={false}
-                title="Student-DB"
-                description="This project is a student management system that allows users to
-                 create, read, update, and delete student records. 
-                Developed using React.js, the user interface is intuitive and responsive, 
-                making it easy for administrators to manage student data."
-                ghLink="https://student-db-crud-app-arun.netlify.app/"
-              />
-            </Col> */}
-            {/* ----------------------------- */}
+
             <Col md={4} className="project-card">
               <ProjectCard
                 imgPath={pending}
@@ -83,18 +87,7 @@ function Projectlist() {
                 aliquip ex ea commodo consequat"
               />
             </Col>
-            {/* ----------------------------- */}
-            <Col md={4} className="project-card">
-              <ProjectCard
-                imgPath={pending}
-                isBlog={false}
-                title="Pending Project"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-                aliquip ex ea commodo consequat"
-              />
-            </Col>
+
             {/* ----------------------------- */}
           </Row>
           {/* ----------------------------- */}
